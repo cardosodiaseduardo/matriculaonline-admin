9823f0634f40834618aecc2a7a5a3685610b8713
cardosodiaseduardo <cardosodiaseduardo@gmail.com>
2020-01-13 22:06:36 -0400

    Deploying Matricula Online

---
+++ precache-manifest.00634216c8498b7dd01ba5dfb9fd5fe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c989206422556c95048",
+    "url": "/matriculaonline-admin/css/chunk-vendors.dc8cd16f.css"
+  },
+  {
+    "revision": "62c2dafaba26c41d6caebf734e7f5cb6",
+    "url": "/matriculaonline-admin/index.html"
+  },
+  {
+    "revision": "0f56e4f6a2cfc645d2b2",
+    "url": "/matriculaonline-admin/js/app.8d3e6885.js"
+  },
+  {
+    "revision": "0c989206422556c95048",
+    "url": "/matriculaonline-admin/js/chunk-vendors.ea6d7fcb.js"
+  },
+  {
+    "revision": "043bc7798fd032a10139d92bce04e490",
+    "url": "/matriculaonline-admin/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/matriculaonline-admin/robots.txt"
+  }
+]);
